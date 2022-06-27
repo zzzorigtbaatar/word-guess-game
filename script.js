c8a1ef21f19629264d36b0868e5a3786f823ed26
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-06-27 14:21:22 -0700

    Add function placeholders

--- script.js
+++ script.js
@@ -2,15 +2,51 @@
 var timerLimit = 60000;
 var timerText = "";
 
+
+//Create object for player?
+
+
+//starts the game
+function init(){
+    resetTimer();
+    displayTimer();
+    displayScores();
+    displaySecretWord();
+}
+
+
 //resets timer to 1 minute/60000 milliseconds
 function resetTimer(){
     timerLimit = 60000;
 }
 
+//displays the current time on timer
+function displayTimer(){
+
+}
+
+//displays the locally stored scores, if there is any.
+function displayScores(){
+
+}
+
+//displays the secret word as underscores initially and then reveals letters if correctly pressed
+function displaySecretWord(){
+
+}
+
+// begins timer until it reaches 0.
 function startTimer(){
     for (var i = timerLimit; i > 0; i - 1000){
-        timerText = "There are " + timerLimit + " milliseconds left!";
+        // timerText = "There are " + timerLimit + " milliseconds left!";
+        document.getElementById("#timer").textContent = "There are " + timerLimit + " milliseconds left!";
     }
-
+    if (timerLimit === 0){
+        document.getElementById("#timer").textContent = "Time's Up!";
+    }
     
 }
+
+
+
+init();
