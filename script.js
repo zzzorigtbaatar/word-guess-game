056600109143d163b88b58c4a84c23ff26c24621
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-06-27 16:06:19 -0700

    update

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 //Initial global variables
 var timerLimit = 60000;
 var timerText = "";
+var currentWord1 = "_";
+var currentWord2 = "_";
+var currentWord3 = "_";
 
 
 //object for player
-var player = {
-    winScore: winScore.value,
-    lossScore: lossScore.value,
-}
+// var player = {
+//     // winScore: winScore.value,
+//     // lossScore: lossScore.value,
+// }
 
 // set object to local storage
 localStorage.setItem("playerStringified", JSON.stringify(player));
 
 //starts the game
 function init() {
-    resetTimer();
-    displayTimer();
-    displayScores();
+    // resetTimer();
+    // displayTimer();
+    // displayScores();
     displaySecretWord();
 }
 
@@ -37,17 +40,21 @@ function displayScores() {
     var latestLossScore = JSON.parse(localStorage.getItem("playerStringified")).lossScore;
 
     if (localStorage.getItem("playerWins") !== null) {
-        document.getElementById("#winsText").textContent = latestWinScore;
-        document.getElementById("#lossesText").textContent = latestLossScore;
+        document.getElementById('winsText').textContent = latestWinScore;
+        document.getElementById('lossesText').textContent = latestLossScore;
     }
 }
 //testing branch stuff here
 //displays the secret word as underscores initially and then reveals letters if correctly pressed
-var secretWord = function displaySecretWord(currentWord) {
+function displaySecretWord() {
+    // var secretWord = 
+    // document.getElementById("saved-name").innerHTML = lastGrade.student;
+    document.getElementById("wordscreen").innerHTML = current1 + currentWord2 + currentWord3;
+    console.log("displaySecretWord is running");
 
 }
 
-document.getElementById("wordscreen").innerHTML = secretWord;
+// document.getElementById("wordscreen").innerHTML = secretWord;
 
 // begins timer until it reaches 0.
 function startTimer() {
@@ -61,6 +68,28 @@ function startTimer() {
 
 }
 
+//checks if game timer can start
+startButton.addEventListener("click", function(event) {
+    startTimer();
+  });
 
+
+
+//checks if keys pressed matches
+document.addEventListener('keydown', logKey);
+
+function logKey(){
+
+    var element = document.target;
+   
+    if (element == "c") {
+      currentWord1 = "C";
+    } else if (element == "a"){
+        currentWord2 = "A";
+    } else if (element == "t"){
+        currentWord3 = "T";
+    }
+
+}
 
 init();
